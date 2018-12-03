a36d99dc7c7f87e25212fbb0bf8f280ea4e90d1c
yenerunver@hotmail.com <yenerunver@boun.edu.tr>
2018-12-03 11:24:12 +0000

    Environment variables

--- tweetoriginalitychecker.py
+++ tweetoriginalitychecker.py
@@ -1,23 +1,28 @@
 import os
 from flask import Flask, request, render_template
+import twitter
 
 app = Flask(__name__)
+
+app.config.from_object('config')
+
+api = twitter.Api(consumer_key=app.config['TWITTER_CONSUMER_KEY'],
+        consumer_secret=app.config['TWITTER_CONSUMER_SECRET'],
+        access_token_key=app.config['TWITTER_ACCESS_TOKEN_KEY'],
+        access_token_secret=app.config['TWITTER_ACCESS_TOKEN_SECRET'],
+        tweet_mode='extended')
 
 @app.route('/')
 def welcome():
     return render_template("welcome.html")
 
-@app.route('/login')
-def login():
-    return render_template("auth/login.html")
+@app.route('/twitterdemo')
+def twitterdemo():
+    return str(api.GetSearch(term='Trump', result_type='popular', count=1, return_json=True))
 
-@app.route('/register')
-def register():
-    return render_template("auth/register.html")
-
-@app.route('/logout')
-def logout():
-    return 0
+@app.route('/test')
+def test():
+    return app.config['SECRET_KEY']
 
 if __name__ == '__main__':
     app.run(debug=True)
